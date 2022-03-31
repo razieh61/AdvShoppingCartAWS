cd72eb66fa9576f90dc70c471ebd092685058d0e
razieh <razieh61>
2022-03-30 17:20:12 -0700

    first commit

---
+++ adshopcart_locators.py
@@ -0,0 +1,31 @@
+import datetime
+
+from faker import Faker
+fake = Faker(locale=['en_CA','en_US'])
+
+#-------- AdvantageShoppingCart App DATA PARAMETERS ----------------
+app = 'AdvantageShoppingCart'
+adshopcart_url = 'https://advantageonlineshopping.com/#/'
+adshopcart_home_page_title = ' Advantage Shopping'
+adshopcart_signup_page_url = 'https://advantageonlineshopping.com/#/register'
+adshopcart_myaccount_url = 'https://advantageonlineshopping.com/#/myAccount'
+adshopcart_myorders_url = 'https://advantageonlineshopping.com/#/MyOrders'
+adshopcart_shoppingcart_url = 'https://advantageonlineshopping.com/#/shoppingCart'
+
+new_username = (fake.user_name())[:15]
+new_password = fake.password(length=12)
+email = fake.email()
+first_name = fake.first_name()
+last_name = fake.last_name()
+full_name = f'{first_name} {last_name}'
+phonenum = fake.pyint(111111111, 999999999)
+country = fake.current_country()
+city = fake.city()
+address = fake.street_address()
+state = fake.province_abbr()
+postal_code = fake.postalcode()
+
+name_list = ['SPEAKERS', 'TABLETS', 'HEADPHONES', 'LAPTOPS', 'MICE']
+id_list = ['speakersTxt', 'tabletsTxt', 'headphonesTxt', 'laptopsTxt', 'miceTxt']
+
+subject = f'I am {full_name}. I need more information about my selected products.\n Thanks {datetime.datetime.now()}'
---
+++ adshopcart_tests.py
@@ -0,0 +1,20 @@
+import unittest
+import adshopcart_locators as locators
+import adshopcart_methods as methods
+
+
+class AdShopCartPositiveTestCases(unittest.TestCase):
+
+    @staticmethod  # signal to unittest that this is a function inside class (vs @classmethod)
+    def test_AdShopCart():  # test_ in the name is mandatory
+        methods.setUp()
+        methods.check_homepage()
+        methods.sign_up()
+        methods.check_full_name()
+        methods.check_orders()
+        methods.log_out()
+        methods.log_in(locators.new_username, locators.new_password)
+        methods.delete_test_account()
+        methods.log_in(locators.new_username, locators.new_password)
+        methods.shoppingcart()
+        methods.tearDown()
